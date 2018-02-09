f9fa2ba4fe5a27e9d63520159a4e233a1218d6c9
gebb <1298525175@qq.com>
2018-02-09 14:15:36 +0800

    'll'

---
+++ h5UI/demos/xxx/js/config.js
@@ -0,0 +1,3 @@
+window.wx_config={debug:false,appId:'wxd2977e7ebfb84861',timestamp:1514945801,nonceStr:'83guvb5m4t992efq3',signature: 'fc5b1807a4277aa9326c024aa625a8684a42b43f', countnum:143998, ksize:1283, status:4};
+window.epub360_userinfo={}
+window.epub360_sourceinfo={}
